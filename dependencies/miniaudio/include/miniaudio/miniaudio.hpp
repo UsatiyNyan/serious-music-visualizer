5043a8aa320d694559cd87bb3f06dfd439591787
Usatiy Nyan <kirillarionov123456@gmail.com>
2023-12-27 02:10:41 +0100

    miniaudio dependency

--- dependencies/miniaudio/include/miniaudio/miniaudio.hpp
+++ dependencies/miniaudio/include/miniaudio/miniaudio.hpp
@@ -3,3 +3,24 @@
 //
 
 #pragma once
+
+#include "miniaudio.h"
+
+#include <memory>
+#include <tl/optional.hpp>
+#include <vector>
+
+namespace ma {
+
+using context_uninit = decltype(&ma_context_uninit);
+using context_uptr = std::unique_ptr<ma_context, context_uninit>;
+context_uptr context_init(std::vector<ma_backend> backends, const ma_context_config& context_config);
+
+using device_uninit = decltype(&ma_device_uninit);
+using device_uptr = std::unique_ptr<ma_device, device_uninit>;
+device_uptr device_init(const context_uptr& context, const ma_device_config& device_config);
+
+tl::optional<tl::monostate> device_start(const device_uptr& device);
+tl::optional<tl::monostate> device_stop(const device_uptr& device);
+
+} // namespace ma
--- dependencies/miniaudio/src/miniaudio.cpp
+++ dependencies/miniaudio/src/miniaudio.cpp
@@ -6,3 +6,32 @@
 
 #define MINIAUDIO_IMPLEMENTATION
 #include "miniaudio.h"
+
+namespace ma {
+
+context_uptr context_init(std::vector<ma_backend> backends, const ma_context_config& context_config) {
+    context_uptr context{ new ma_context, &ma_context_uninit };
+    const auto* backends_ptr = backends.empty() ? nullptr : backends.data();
+    if (ma_context_init(backends_ptr, backends.size(), &context_config, context.get()) != MA_SUCCESS) {
+        context.reset();
+    }
+    return context;
+}
+
+device_uptr device_init(const context_uptr& context, const ma_device_config& device_config) {
+    device_uptr device{ new ma_device, &ma_device_uninit };
+    if (ma_device_init(context.get(), &device_config, device.get()) != MA_SUCCESS) {
+        device.reset();
+    }
+    return device;
+}
+
+tl::optional<tl::monostate> device_start(const device_uptr& device) {
+    return ma_device_start(device.get()) == MA_SUCCESS ? tl::optional{ tl::monostate{} } : tl::nullopt;
+}
+
+tl::optional<tl::monostate> device_stop(const device_uptr& device) {
+    return ma_device_stop(device.get()) == MA_SUCCESS ? tl::optional{ tl::monostate{} } : tl::nullopt;
+}
+
+} // namespace ma
