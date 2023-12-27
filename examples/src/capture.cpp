9ee99d4092058b9c569bd39330e953e4c8932917
Usatiy Nyan <kirillarionov123456@gmail.com>
2023-12-27 02:11:02 +0100

    miniaudio: capture and sine_wave examples

---
+++ examples/src/capture.cpp
@@ -0,0 +1,66 @@
+//
+// Created by usatiynyan on 12/27/23.
+//
+// TODO(@usatiynyan): fft transform -> visualize
+//
+
+#include "miniaudio/miniaudio.hpp"
+
+#include <assert.hpp>
+#include <fmt/format.h>
+
+#include <atomic>
+#include <chrono>
+#include <thread>
+
+int main() {
+    const ma_context_config context_config = ma_context_config_init();
+    const auto context = ASSERT(ma::context_init({}, context_config));
+    fmt::println("context backend: {}", ma_get_backend_name(context->backend));
+
+    constexpr auto data_callback =
+        [](ma_device* device, [[maybe_unused]] void* output, const void* input, ma_uint32 frame_count) {
+            ASSERT(device->capture.format == ma_format_f32);
+            const float* input_f32 = static_cast<const float*>(input);
+
+            float curr_highest_amplitude = 0;
+            for (std::uint32_t frame_index = 0; frame_index < frame_count; ++frame_index) {
+                for (std::uint32_t channel_index = 0; channel_index < device->capture.channels; ++channel_index) {
+                    const float amplitude = input_f32[frame_index * device->capture.channels + channel_index];
+                    curr_highest_amplitude = std::max(amplitude, curr_highest_amplitude);
+                }
+            }
+
+            auto* highest_amplitude = ASSERT(static_cast<std::atomic<std::float_t>*>(device->pUserData));
+            highest_amplitude->store(curr_highest_amplitude, std::memory_order::release);
+        };
+
+    constexpr ma_uint32 device_channels = 2;
+    constexpr ma_uint32 device_sample_rate = 48000;
+    const auto highest_amplitude = std::make_unique<std::atomic<std::float_t>>();
+
+    ma_device_config device_config = ma_device_config_init(ma_device_type_capture);
+    device_config.capture.format = ma_format_f32;
+    device_config.capture.channels = device_channels;
+    device_config.sampleRate = device_sample_rate;
+    device_config.dataCallback = data_callback;
+    device_config.pUserData = highest_amplitude.get();
+
+    const auto device = ASSERT(ma::device_init(context, device_config));
+    fmt::println("Device Name: {}", device->playback.name);
+
+    ASSERT(ma::device_start(device));
+
+    fmt::println("Press Enter to quit...");
+    while (true) {
+        std::this_thread::sleep_for(std::chrono::milliseconds{ 33 });
+        const float curr_highest_amplitude = highest_amplitude->load(std::memory_order::acquire);
+        const std::size_t stars_count = static_cast<std::size_t>(curr_highest_amplitude * 256.0f);
+        for (std::size_t i = 0; i < stars_count; ++i) {
+            fmt::print("*");
+        }
+        fmt::println("|");
+    }
+
+    return 0;
+}
---
+++ examples/src/sine_wave.cpp
@@ -0,0 +1,47 @@
+//
+// Created by usatiynyan on 12/26/23.
+//
+// TODO(@usatiynyan): fft transform -> visualize
+//
+
+#include "miniaudio/miniaudio.hpp"
+
+#include <assert.hpp>
+#include <fmt/format.h>
+
+int main() {
+    const ma_context_config context_config = ma_context_config_init();
+    const auto context = ASSERT(ma::context_init({}, context_config));
+
+    constexpr auto data_callback =
+        [](ma_device* device, void* output, [[maybe_unused]] const void* input, ma_uint32 frame_count) {
+            auto* sine_wave = ASSERT(static_cast<ma_waveform*>(device->pUserData));
+            ma_waveform_read_pcm_frames(sine_wave, output, frame_count, nullptr);
+        };
+
+    constexpr ma_uint32 device_channels = 2;
+    constexpr ma_uint32 device_sample_rate = 48000;
+    const auto sine_wave = std::make_unique<ma_waveform>();
+
+    ma_device_config device_config = ma_device_config_init(ma_device_type_playback);
+    device_config.playback.format = ma_format_f32;
+    device_config.playback.channels = device_channels;
+    device_config.sampleRate = device_sample_rate;
+    device_config.dataCallback = data_callback;
+    device_config.pUserData = sine_wave.get();
+
+    const auto device = ASSERT(ma::device_init(context, device_config));
+    fmt::println("device name: {}", device->playback.name);
+
+    ma_waveform_config sineWaveConfig = ma_waveform_config_init(
+        device->playback.format, device->playback.channels, device->sampleRate, ma_waveform_type_sine, 0.2, 220
+    );
+    ma_waveform_init(&sineWaveConfig, sine_wave.get());
+
+    ASSERT(ma::device_start(device));
+
+    fmt::println("press enter to quit...");
+    std::getchar();
+
+    return 0;
+}
