554791c18133ba51804560936fdb98a1002ea8c2
Usatiy Nyan <kirillarionov123456@gmail.com>
2024-01-03 02:37:17 +0100

    dependencies: fix tl

--- dependencies/tl.cmake
+++ dependencies/tl.cmake
@@ -6,3 +6,12 @@ cpmaddpackage(
         GIT_TAG v1.1.0)
 unset(OPTIONAL_BUILD_TESTS)
 unset(OPTIONAL_BUILD_PACKAGE)
+
+set(EXPECTED_BUILD_TESTS OFF)
+set(EXPECTED_BUILD_PACKAGE OFF)
+cpmaddpackage(
+        NAME tl-expected
+        GIT_REPOSITORY "git@github.com:TartanLlama/expected.git"
+        GIT_TAG v1.1.0)
+unset(EXPECTED_BUILD_TESTS)
+unset(EXPECTED_BUILD_PACKAGE)
