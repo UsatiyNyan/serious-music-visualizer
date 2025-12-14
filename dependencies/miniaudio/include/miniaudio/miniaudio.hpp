37a46e4842c6715961fb745f2b0ed0ae2df1c41d
UsatiyNyan <us4tiyny4n@gmail.com>
2025-12-14 22:32:53 +0100

    begin 1.0.0: fix deps

--- dependencies/miniaudio/include/miniaudio/miniaudio.hpp
+++ dependencies/miniaudio/include/miniaudio/miniaudio.hpp
@@ -1,12 +1,13 @@
 //
-// Created by usatiynyan on 12/22/23.
+// Created by usatiynyan
 //
 
 #pragma once
 
 #include "miniaudio.h"
 
-#include <tl/expected.hpp>
+#include <sl/meta/monad/result.hpp>
+#include <sl/meta/type/unit.hpp>
 
 #include <memory>
 #include <span>
@@ -19,7 +20,7 @@ std::string_view get_backend_name(ma_backend backend);
 
 using context_uninit = decltype(&ma_context_uninit);
 using context_uptr = std::unique_ptr<ma_context, context_uninit>;
-tl::expected<context_uptr, ma_result>
+sl::meta::result<context_uptr, ma_result>
     context_init(const std::vector<ma_backend>& backends, const ma_context_config& context_config);
 
 struct context_get_devices_result_t {
@@ -27,13 +28,14 @@ struct context_get_devices_result_t {
     std::span<ma_device_info> capture_infos;
 };
 
-tl::expected<context_get_devices_result_t, ma_result> context_get_devices(const context_uptr& context);
+sl::meta::result<context_get_devices_result_t, ma_result> context_get_devices(const context_uptr& context);
 
 using device_uninit = decltype(&ma_device_uninit);
 using device_uptr = std::unique_ptr<ma_device, device_uninit>;
-tl::expected<device_uptr, ma_result> device_init(const context_uptr& context, const ma_device_config& device_config);
+sl::meta::result<device_uptr, ma_result>
+    device_init(const context_uptr& context, const ma_device_config& device_config);
 
-tl::expected<tl::monostate, ma_result> device_start(const device_uptr& device);
-tl::expected<tl::monostate, ma_result> device_stop(const device_uptr& device);
+sl::meta::result<sl::meta::unit, ma_result> device_start(const device_uptr& device);
+sl::meta::result<sl::meta::unit, ma_result> device_stop(const device_uptr& device);
 
 } // namespace ma
--- dependencies/miniaudio/src/miniaudio.cpp
+++ dependencies/miniaudio/src/miniaudio.cpp
@@ -17,11 +17,11 @@ std::string_view get_backend_name(ma_backend backend) { return ma_get_backend_na
 #define MA_UNEXPECTED(x)                                    \
     do {                                                    \
         if (ma_result result = (x); result != MA_SUCCESS) { \
-            return tl::make_unexpected(result);             \
+            return sl::meta::err(result);                   \
         }                                                   \
     } while (false)
 
-tl::expected<context_uptr, ma_result>
+sl::meta::result<context_uptr, ma_result>
     context_init(const std::vector<ma_backend>& backends, const ma_context_config& context_config) {
     context_uptr context{ new ma_context, &ma_context_uninit };
     const auto* backends_ptr = backends.empty() ? nullptr : backends.data();
@@ -29,7 +29,7 @@ tl::expected<context_uptr, ma_result>
     return context;
 }
 
-tl::expected<context_get_devices_result_t, ma_result> context_get_devices(const context_uptr& context) {
+sl::meta::result<context_get_devices_result_t, ma_result> context_get_devices(const context_uptr& context) {
     ma_device_info* playback_infos;
     ma_uint32 playback_count;
     ma_device_info* capture_infos;
@@ -43,20 +43,21 @@ tl::expected<context_get_devices_result_t, ma_result> context_get_devices(const 
     };
 }
 
-tl::expected<device_uptr, ma_result> device_init(const context_uptr& context, const ma_device_config& device_config) {
+sl::meta::result<device_uptr, ma_result>
+    device_init(const context_uptr& context, const ma_device_config& device_config) {
     device_uptr device{ new ma_device, &ma_device_uninit };
     MA_UNEXPECTED(ma_device_init(context.get(), &device_config, device.get()));
     return device;
 }
 
-tl::expected<tl::monostate, ma_result> device_start(const device_uptr& device) {
+sl::meta::result<sl::meta::unit, ma_result> device_start(const device_uptr& device) {
     MA_UNEXPECTED(ma_device_start(device.get()));
-    return tl::monostate{};
+    return sl::meta::unit{};
 }
 
-tl::expected<tl::monostate, ma_result> device_stop(const device_uptr& device) {
+sl::meta::result<sl::meta::unit, ma_result> device_stop(const device_uptr& device) {
     MA_UNEXPECTED(ma_device_stop(device.get()));
-    return tl::monostate{};
+    return sl::meta::unit{};
 }
 
 #undef MA_UNEXPECTED
